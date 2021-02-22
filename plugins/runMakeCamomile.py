7be0a4601e21646c0fad29146b36d341301389d8
aviloff <53415673+aviloff@users.noreply.github.com>
2021-02-22 17:27:35 +0100

    leave only one plugin

--- plugins/runMakeCamomile.py
+++ plugins/runMakeCamomile.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-# import time
 import shutil 
 import os
 import distutils
@@ -10,21 +9,8 @@ DIR=os.path.dirname(os.path.realpath(__file__))
 CamomileRoot='/Users/Shared/CamomileBin'
 CamomileDir=CamomileRoot+'/Examples'
 Vst3PluginFolder='/Library/Audio/Plug-Ins/VST3'
-# print(os.listdir(DIR))
 
 for PluginName in os.listdir(DIR):
     SourceCamomile=DIR+'/'+PluginName
     if os.path.isdir(SourceCamomile):
-
-        # distutils.dir_util.copy_tree(SourceCamomle, CamomileDir+'/'+PluginName) 
-        # print('cd '+CamomileRoot+' && ./camomile -f '+SourceCamomile+' -o '+Vst3PluginFolder+'/CamomileUtils'+' -v')
         os.system('cd '+CamomileRoot+' && ./camomile -f '+SourceCamomile+' -o '+Vst3PluginFolder+'/CamomileUtils'+' -v') 
-        # os.system('cd '+CamomileRoot+' && ./camomile') 
-
-        # shutil.rmtree(CamomileDir+'/'+PluginName)
-        
-        # print(CamomileRoot+'/builds/'+PluginName+'.vst3')
-
-        # distutils.dir_util.copy_tree(CamomileRoot+'/builds/'+PluginName+'.vst3', Vst3PluginFolder+'/CamomileUtils/'+PluginName+'.vst3') 
-        # shutil.rmtree(CamomileRoot+'/builds')
-
