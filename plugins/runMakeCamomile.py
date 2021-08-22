f377cd764d656c610116f1c3a1dbd779c03b8d32
aviloff <53415673+aviloff@users.noreply.github.com>
2021-08-22 11:54:46 +0300

    sends

---
+++ plugins/runMakeCamomile.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import shutil 
+import os
+import distutils
+from distutils import dir_util
+
+DIR=os.path.dirname(os.path.realpath(__file__))
+# CamomileRoot='/Users/Shared/CamomileBin'
+CamomileRoot='/Applications/Camomile108'
+Vst3PluginFolder='/Library/Audio/Plug-Ins/VST3'
+
+for PluginName in os.listdir(DIR):
+    SourceCamomile=DIR+'/'+PluginName
+    if os.path.isdir(SourceCamomile):
+        os.system('cd '+CamomileRoot+' && ./camomile -f '+SourceCamomile+' -o '+Vst3PluginFolder+'/CamomileUtils'+' -vst3') 
